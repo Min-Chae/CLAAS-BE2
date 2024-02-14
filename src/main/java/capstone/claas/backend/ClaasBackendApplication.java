ed713f86211e0ac8fced0c4b8aba607166ef5dd0
su-giana <giananews@gmail.com>
2024-02-14 14:46:27 +0900

    initial commit

---
+++ src/main/java/capstone/claas/backend/ClaasBackendApplication.java
@@ -0,0 +1,13 @@
+package capstone.claas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaasBackendApplication.class, args);
+    }
+
+}
